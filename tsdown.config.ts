4d961bb9a58309a5f3b0c2cf888c6465edba4052
Veeranki Jitendra <jitendra@ratnamsolutions.com>
2026-04-16 21:34:43 +0530

    fixing types issue

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "tsdown";
+import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  outDir: "dist",
-  format: ["esm", "cjs"],
-  dts: true,
+  entry: ['src/index.ts'],
+  outDir: 'dist',
+  format: ['esm', 'cjs'],
+  dts: false, // ❗ disable tsdown types
   clean: true,
 });
